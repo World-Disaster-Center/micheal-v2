510146adb33e22c1528499c68c7a11eec397e73c
Doddy Matabaro <matabarododdy@gmail.com>
2025-01-08 18:35:57 +0200

    update button pred

--- client/src/components/features/PredictionDrawer.jsx
+++ client/src/components/features/PredictionDrawer.jsx
@@ -23,7 +23,7 @@ const PredictionDrawer = ({ isOpen, onClose }) => {
   };
 
   return (
-    <CustomDrawer isOpen={isOpen} onClose={onClose} title="Prediction Alerts" width="w-2/6">
+    <CustomDrawer isOpen={isOpen} onClose={onClose} title="Prediction Alerts" width="w-1/5">
       <div className="p-4 bg-gray-50 rounded-md shadow-md">
         <p className="text-red-700 font-semibold mb-2">
           Selected Location:{" "}
@@ -51,7 +51,7 @@ const PredictionDrawer = ({ isOpen, onClose }) => {
             onClick={handleFetchData}
             className="w-1/3 bg-red-700 text-white font-medium rounded-md p-3 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-700 focus:ring-opacity-50"
           >
-            Fetch Data
+            Search
           </button>
         )}
         </div>
