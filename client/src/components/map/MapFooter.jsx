d98e3f2e77f0f65ef27a8c6072adb865fbe87271
Razac <DerickZIHALIRWA07@gmail.com>
2025-01-08 14:59:47 +0300

    fix: conflict

--- client/src/components/map/MapFooter.jsx
+++ client/src/components/map/MapFooter.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchDisastersByCategory } from '../../redux/services/disasters';
+import MapTypeModal from './MapTypeModal';
 import naturalIcon from '../../assets/natural1.png'
 import chatIcon from '../../assets/chatIcon.png'
 import mapIcon from '../../assets/mapIcon.png'
 import translate from '../../assets/translate.png'
-import demonstrationIcon from '../../assets/demonst1.png'
 import droughtIcon from '../../assets/drought1.png'
 import earthquakeIcon from '../../assets/earthquake1.png'
 import floodIcon from '../../assets/flood1.png'
@@ -13,47 +15,58 @@ import technologicalIcon from '../../assets/tech1.png'
 import tropicalCyclone from '../../assets/cyclone1.png';
 
 import ChatAIFloater from '../features/chatFloater';
+import wildFire from '../../assets/wildfire.png'
+
 
 const buttonDisasters = [
     {
-        "name": "Natural",
-        "img": naturalIcon
+        name: "Volcanoes",
+        img: naturalIcon,
+        id: "volcanoes"
     },
     {
-        "name": "Demonstration",
-        "img": demonstrationIcon
+        name: "Wildfires",
+        img: wildFire,
+        id: "wildfires"
+    },
+    
+    {
+        name: "Drought",
+        img: droughtIcon,
+        id: "drought"
     },
     {
-        "name": "Drought",
-        "img": droughtIcon
+        name: "Earthquake",
+        img: earthquakeIcon,
+        id: "earthquakes"
     },
     {
-        "name": "Earthquake",
-        "img": earthquakeIcon
+        name: "Flood",
+        img: floodIcon,
+        id: "floods"
     },
     {
-        "name": "Flood",
-        "img": floodIcon
+        name: "Violences",
+        img: politicalIcon
     },
     {
-        "name": "Violences",
-        "img": politicalIcon
+        name: "Development",
+        img: strategicIcon,
+        id: "dustHaze"
     },
     {
-        "name": "Development",
-        "img": strategicIcon
+        name: "Technological",
+        img: technologicalIcon,
+        id: "manmade"
     },
     {
-        "name": "Technological",
-        "img": technologicalIcon
-    },
-    {
-        "name": "Cyclones",
-        "img": tropicalCyclone
+        name: "Cyclones",
+        img: tropicalCyclone,
+        id: "severeStorms"
     }
 ]
 
-const MapFooter = () => {
+const MapFooter = ({ onMapTypeChange }) => {
   const [chatOpen, setChatOpen] = useState(false);
   const [userLocation, setUserLocation] = useState(null);
   const [openPanel, setOpenPanel] = useState(false);
@@ -89,36 +102,67 @@ const MapFooter = () => {
 
 
 
+    const dispatch = useDispatch();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const mapTypeButtonRef = useRef(null);
+
+    const handleCategoryClick = (categoryId) => {
+        if (categoryId) {
+            dispatch(fetchDisastersByCategory(categoryId));
+        }
+    };
+
+    const handleMapTypeSelect = (mapType) => {
+        if (onMapTypeChange) {
+            onMapTypeChange(mapType);
+        }
+    };
+
     return (
         <div className='fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-screen-xl mx-auto px-4 flex items-center justify-center'>
-
             <div className='flex gap-4 mx-10 my-10'>
+                <button onClick={toggleChat} className='flex items-center justify-center bg-white h-[50px] w-[50px] rounded-full border-gray-300 shadow-lg hover:bg-slate-300 transition-all duration-400'>
+                    <img className='' src={chatIcon} alt="Chat" />
+                </button>
+                <button
+                  ref={mapTypeButtonRef}
+                  onClick={() => setIsModalOpen(!isModalOpen)}
+                  className='flex items-center justify-center bg-white h-[50px] w-[50px] rounded-full border-gray-300 shadow-lg hover:bg-slate-300 transition-all duration-400'
+                >
+                    <img src={mapIcon} alt="Map type" />
+                </button>
+            </div>
 
-                <button onClick={toggleChat} className='flex items-center justify-center bg-white h-[50px] w-[50px] rounded-full border-gray-300  shadow-lg hover:bg-slate-300 transition-all duration-400'>
-                    <img className='' src={chatIcon} />
-                </button>
-                <button className='flex items-center justify-center bg-white h-[50px] w-[50px] rounded-full border-gray-300  shadow-lg hover:bg-slate-300 transition-all duration-400'>
-                    <img src={mapIcon} />
-                </button>
-            </div>
             <div className='flex gap-4 my-10'>
-                {buttonDisasters.map((item, index) => (
-                    <button key={index} className='flex items-center justify-center py-1 text-sm font-light gap-3 px-5 bg-slate-50 border border-gray-300 rounded-full shadow-lg hover:bg-slate-300 transition-all duration-400'>
-                        <img className='h-[20px] w-[20px]' src={item.img} alt='' />
-                        <span className='text-[12px] font-semibold'>{item.name}</span>
+                {buttonDisasters.map((button) => (
+                    <button
+                        key={button.name}
+                        onClick={() => handleCategoryClick(button.id)}
+                        className='flex items-center justify-center py-1 text-sm font-light gap-3 px-5 bg-slate-50 border border-gray-300 rounded-full shadow-lg hover:bg-slate-300 transition-all duration-400'
+                    >
+                        <img className='h-[20px] w-[20px]' src={button.img} alt={button.name} />
+                        <span className='text-[12px] font-semibold'>{button.name}</span>
                     </button>
-                ))} 
+                ))}
             </div>
+
             <div className='flex gap-4 mx-10 my-10'>
                 <button  className='flex items-center justify-center h-[50px] w-[50px] rounded-sm shadow-xl hover:bg-gray-400 transition-all duration-500'>
                     <img
-                    className='' src={translate} />
+                    className='' src={translate} alt="Translate" />
                 </button>
             </div>
+
+            <MapTypeModal 
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onSelectMapType={handleMapTypeSelect}
+                buttonRef={mapTypeButtonRef}
+            />
              <ChatAIFloater chatOpen={chatOpen} userLocation={userLocation} riskNotification={riskNotification}/>
              
         </div>
-    )
-}
+    );
+};
 
 export default MapFooter
